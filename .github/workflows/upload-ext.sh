f53ee8ecd2209b178fb2d0c1a2366f2d617a98bb
Boonyarith Piriyothinkul <boonyarith.pir@gmail.com>
2022-01-27 00:06:32 +0700

    publish ext through gha

---
+++ .github/workflows/upload-ext.sh
@@ -0,0 +1,56 @@
+#!/bin/sh
+
+set -e
+
+token=`curl \
+--silent \
+--fail \
+-H "Content-Type: application/json" \
+-d '{
+	"refresh_token": "'$1'",
+	"client_id": "'$2'",
+	"client_secret": "'$3'",
+	"grant_type": "refresh_token"
+}' \
+-X POST \
+-v https://www.googleapis.com/oauth2/v4/token \
+| \
+jq -r '.access_token'`
+
+status=`curl \
+--silent \
+--show-error \
+--fail \
+-H "Authorization: Bearer $token" \
+-H "x-goog-api-version: 2" \
+-X PUT \
+-T extension.zip \
+-v https://www.googleapis.com/upload/chromewebstore/v1.1/items/icinedakgbcmhanlhfhckigdeefompfg \
+| \
+jq -r '.uploadState'`
+
+if [ $status == 'FAILURE' ]
+then
+  exit 1
+fi
+
+  publish=`curl \
+  --silent \
+  --show-error \
+  --fail \
+  -H "Authorization: Bearer $token" \
+  -H "x-goog-api-version: 2" \
+  -X POST \
+  -T extension.zip \
+  -v https://www.googleapis.com/upload/chromewebstore/v1.1/items/icinedakgbcmhanlhfhckigdeefompfg/publish \
+  -d publishTarget=default \
+  | \
+  jq -r '.publishState'`
+
+  if [ $publish == 'FAILURE' ]
+  then
+    exit 1
+  fi
+fi
+
+exit 0
